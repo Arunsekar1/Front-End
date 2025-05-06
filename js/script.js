5dbbc1288cf040d4c82d5013fc5e2b5e4e80ee97
Arun <arunsekar071@gmail.com>
2025-05-06 12:05:30 +0530

    6.5.25

--- js/script.js
+++ js/script.js
@@ -16,9 +16,123 @@
 // console.log(Name);
 
 
-const Num = 123456;
+// const Num = 123456;
 
 // Num=11223
 
-console.log(Num);
+// console.log(Num);
+
+//  alert('warning')
+// prompt('Enter Your Name')
+// confirm(' r u exit!')
+
+//  open('https://idmtechpark-mern.netlify.app/')
+//  close()
+
+// print()
+
+// let num = 10 ;
+
+// console.log(num);
+// console.log(num.toString());
+
+// let n = "20";
+
+// console.log(n);
+// console.log(parseInt(n));
+
+
+// let Name = " Suryraj "
+// console.log(Name);
+// console.log(Name.length);
+// console.log(Name.concat("Bharath"));
+// console.log(Name+" Bhrath");
+// console.log(Name.concat(Name));
+// console.log(Name.repeat(4));
+// console.log(Name.toUpperCase());
+// console.log(Name.trim());
+
+
+// console.log(Name[1]);
+// console.log(Name.indexOf("r"));
+// console.log(Name.lastIndexOf('r'));
+
+// Name[9]="bharath"
+// console.log(Name);
+
+// console.log(Math.E);
+// console.log(Math.PI);
+// console.log(Math.abs(90));
+// console.log(Math.ceil(9.1));
+// console.log(Math.floor(999.999990));
+// console.log(Math.max(99,77));
+// console.log(Math.min(68,98));
+// console.log(Math.pow(2,5));
+// console.log(Math.random());
+// console.log(Math.round(4.5));
+// console.log(Math.sign(-9));
+
+
+
+// +,-,*,/,%
+
+// == !=
+
+// < > <= >= != == ===
+
+// AND &&  OR||  NOT !
+
+// ++  --
+
+// 0 1 AND OR NOT 
+
+// value ? true : false 
+
+// if(20<4){
+//     console.log("1st value crt..");
+    
+// }
+
+// else if((10<50)&&(60<7)){
+//     console.log("2nd Crt..");
+    
+// }
+
+// else if((8<5)||(9<7)){
+// console.log("3rd value is crt,..");
+
+// }
+
+// else{
+//     console.error("Faill");
+    
+// }
+
+
+// if(90<99){
+//     if(80<90){
+//         if(70<99){
+//             console.log(":Success");
+            
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
