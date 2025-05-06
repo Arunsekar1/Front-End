eb77ff333c10ccfe1656e3f9ef1e83c17ed17fe5
Arun <arunsekar071@gmail.com>
2025-05-06 10:39:49 +0530

    js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// confirm('r u exit..!')
+
+// console.log("Hello Welcome..");
+
+// document.write('<h1>Hello surya</h1>')
+
+
+// var Name = "Surya";
+
+// var Name = "Bharath"
+
+// let Name = "Surya";
+
+// Name ="Bharath" 
+
+// console.log(Name);
+
+
+const Num = 123456;
+
+// Num=11223
+
+console.log(Num);
+
